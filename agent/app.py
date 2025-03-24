feb23873c1c565084453aab8214d16642d4d2d77
Cian Fish <cianfish@Cians-MacBook-Air.local>
2025-03-24 18:23:35 +0000

    MAJOR UPDATE - Grafana Webhook working with LLM

--- agent/app.py
+++ agent/app.py
@@ -3,7 +3,8 @@ from models import LLMQuery, GrafanaMessage, APILLMQuery
 from ai_agent import Agent
 
 api = FastAPI()
-chat_agent = Agent()
+agent = Agent()
+script_running = 0
 
 @api.get("/")
 def read_root():
@@ -13,22 +14,21 @@ def read_root():
 @api.post("/llm")
 def llm(question: APILLMQuery):
     llm_question = LLMQuery(question=question.question)
-    response = chat_agent.alert(llm_message=llm_question)
+    response = agent.alert(llm_message=llm_question)
     return response
 
 @api.post("/alert")
 async def alerts(payload: GrafanaMessage):
-    counter = 0
-
+    global script_running
     if payload.status == "firing":
-        if counter == 0: 
-            #Call function to call LLM 
-            print(payload.commonAnnotations["description"])
-            counter += 1
-        if counter >= 1:
-            return "Already working with LLM."
+        if script_running == 0: 
+            agent.alert(llm_message=payload.commonAnnotations["description"])
+            script_running += 1
+            return {"message": "Alert received and processing."}
+        elif script_running >= 1:
+            return {"message": "Alert already being processed."}
 
     elif payload.status == "resolved":
-        # Need to figure out if I can cancel / notify the LLM to stop if not already.
-        print("Resolved")
-        counter = 0
+        script_running = 0
+        return {"message": "Alert resolved."}
+        
