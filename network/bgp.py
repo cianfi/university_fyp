eb61406959e1bfc2e90af5f81d28b5f3b1de0bb9
Cian Fish <cianfish@Cians-MacBook-Air.local>
2025-03-26 19:00:03 +0000

    Update - PyATS format test

--- network/bgp.py
+++ network/bgp.py
@@ -10,12 +10,7 @@ bgp_configuration: dict = {
     "router-1": [
         "no router ospf 1",
         "no router bgp 1",
-        "router bgp 1",
-        ["router bgp 1", "bgp router-id 100.100.100.1"],
-        ["router bgp 1", "bgp log-neighbor-changes"],
-        ["router bgp 1", "network 5.5.5.0 mask 255.255.255.0"],
-        ["router bgp 1", "network 6.6.6.0 mask 255.255.255.0"],
-        ["router bgp 1", "neighbor 100.100.100.2 remote-as 2"],
+        "router bgp 1\nbgp router-id 100.100.100.1\nbgp log-neighbor-changes\nnetwork 5.5.5.0 mask 255.255.255.0\nnetwork 6.6.6.0 mask 255.255.255.0\nneighbor 100.100.100.2 remote-as 2",
     ],
     "router-2": [
         "no router ospf 1",
@@ -35,5 +30,6 @@ for device_raw in bgp_configuration:
 
     for command in bgp_configuration[device_raw]:
         device.configure(command)
+    print()
     
     device.disconnect()
